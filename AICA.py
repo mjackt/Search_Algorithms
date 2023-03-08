b2de08ba6f24606ec5deff4df233b0fffa8ee32d
mjackt <mjackt@outlook.com>
2023-03-08 16:56:02 +0000

    Added comments and readme

--- AICA.py
+++ AICA.py
@@ -1,14 +1,16 @@
+#used to measure process time
 from time import process_time
-import math
+#used to find files
 import glob
 
-#takes array returns index with a dash
+#takes array of characters -> returns index with a '-'
 def findDash(line):
     for i in range(0,len(line)):
         if line[i] == "-":
             return i
 
-#Takes a graph in the form of a dictionary as created by mazeToDict
+#Takes a graph in the form of a dictionary as created by mazeToDict as well as the start and goal nodes
+#Returns the path and number of nodes explored, both in a tuple
 def depthFirstSearch(graph,start,goal):
     explored = 0
     visited = set()
@@ -25,9 +27,10 @@ def depthFirstSearch(graph,start,goal):
             if current == goal:
                 return (route,explored)
             visited.add(current)
+            #Every discovered neighbour goes to the top of the stack
             for neighbor in graph[current]:
                 stack.append((neighbor, route + [neighbor]))
-    #if the stack becomes empty every accesible node has been visited and no path has been found
+    #if the stack becomes empty every, accesible node has been visited and no path has been found
     return None
 
 def greedySearch(graph,start,goal):
@@ -41,24 +44,28 @@ def greedySearch(graph,start,goal):
 
     while len(toLook) != 0:
         explored += 1
-        #takes first element which will always be smallest distance
+        #pops first element which will always have the smallest estimate distance
         (current, route, n) = toLook.pop(0)
         if current not in visited:
             if current == goal:
                 return (route,explored)
             visited.add(current)
             for neighbor in graph[current]:
+                #absx and absy are the absolute values of the differnce in x and y values between the current and goal nodes
                 absx = abs(goal[0]-current[0])
                 absy = abs(goal[1]-current[1])
                 distance = absx + absy
 
+                #inserting discovered node into toLook using the estimate distance as a sorting key
                 toLook = insertToListOfTuples(toLook,(neighbor, route + [neighbor],distance))
                                      
     #if toLook becomes empty every accesible node has been visited and no path has been found
     return None
 
-#takes tuples with (any,any,int) and sorts by int value
+#takes a sorted list of tuples with type(any,any,int) and and a tuple of the same type to insert
+#returns the list with the tuple inserted
 def insertToListOfTuples(list,insertion):
+    #iterating up the list until a value bigger than the insert value is found. Same as any standard insertion algrotithm
     for i in range(0,len(list)):
         if insertion[2]<list[i][2]:
             list.insert(i,insertion)
@@ -68,13 +75,16 @@ def insertToListOfTuples(list,insertion):
         list.insert(0,insertion)
         return list
     
+    #if this point is reached it means the insert tuple has the largest value so must be appended
     list.append(insertion)
     return list
         
-
+#takes a filename for a relevant maze
 #returns tuple containing a dictionary, and coords of start and end
 #dictionary uses a key for every position that has a - in the maze
 #each key's value is an array of accessible neighbours
+#Example:
+#[3,2] : [[3,3],[4,2]] 
 def mazeToDict(filename):
     #Each row is an array so accesing means you need to flip coords.
     # i.e [y][x]
@@ -83,6 +93,8 @@ def mazeToDict(filename):
     for line in file:
         Coord2DArray.append(line.split())
     #Now we have a full 2D array of coords
+
+    #Creating start and goal coords using findDash()
     startx = findDash(Coord2DArray[0])
     start = (startx,0)
 
@@ -94,10 +106,12 @@ def mazeToDict(filename):
     
     for y in range(0,len(Coord2DArray)):
         for x in range(0,len(Coord2DArray[y])):
+            #If a dash is found it is added to the dictionary
             if Coord2DArray[y][x] == "-":
+                #The next step is adding all of it's neighbours as values to the key in the dictionary.
                 dictionary[(x,y)] = []
 
-                #multiple ifs prevent accessing errors
+                #multiple ifs prevent accessing errors. don't touch
                 #above
                 if y>0:
                     if Coord2DArray[y-1][x]=="-":
@@ -115,25 +129,31 @@ def mazeToDict(filename):
                     if Coord2DArray[y][x+1]=="-":
                         dictionary[(x,y)].append((x+1,y))
 
+            #if to detect invalid file formats
             if Coord2DArray[y][x] not in ["-","#"," "]:
                 raise Exception()
     return (dictionary,start,goal)
 
 #path is the result from the search
 #origin maze is the original .txt file
+#outFile is the desired output file name
+#nothing is returned
 def outputMaze(path,originMaze,outFile):
     file = open(originMaze)
 
-    #is a 2D array where each array is a line - access backwards i.e [y][x] not [x][y]
+    #rows is a 2D array where each array is a line - access backwards i.e [y][x] not [x][y]
     rows = []
+    #splits each line into array of characters excluding spaces and adds it to rows
     for line in file:
         chars = line.split()
         rows.append(chars)
+    #goes through path and updates every path coord to a ? in the text representation of the maze
     for i in range(0,len(path)):
         currentx = path[i][0]
         currenty = path[i][1]
         rows[currenty][currentx] ="?"
-    #writing to .txt section
+
+
     searchResult = open("results/"+outFile[6:],'w')
     #taking each row of characters and converting back to one string seperated by spaces
     for line in rows:
